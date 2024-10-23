2078e2a8c0f8443972984ce2510a1c2ab558191b
Irawanteguh <teguhirawan.rsudpasarminggu@gmail.com>
2024-10-23 16:20:41 +0700

    Update .env

--- application/hooks/EnvironmentLoader.php
+++ application/hooks/EnvironmentLoader.php
@@ -12,7 +12,7 @@
             self::$appInstance = get_instance();
             self::$appInstance->load->model("Modelroot");
             
-            $orgId = isset($_SESSION['orgid']) ? $_SESSION['orgid'] : '378a0728-a914-43a5-ae90-186d8a052ff2';
+            $orgId = isset($_SESSION['orgid']) ? $_SESSION['orgid'] : getenv('ORGID');
             self::$environmentSettings = self::$appInstance->Modelroot->environment($orgId);
 
             if (!empty(self::$environmentSettings)) {
