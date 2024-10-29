1bab1015ab53379713ecef99c1fb09a5b8554943
Irawanteguh <teguhirawan.rsudpasarminggu@gmail.com>
2024-10-29 14:02:27 +0700

    Update

--- application/modules/tilaka/controllers/Registrasi.php
+++ application/modules/tilaka/controllers/Registrasi.php
@@ -384,47 +384,63 @@
         }
 
         public function reenroll(){
-            $useridentifier    = $this->input->post("useridentifier");
-            $consent_timestamp = date("Y-m-d H:i:s");
-            $consent_text      = "Syarat dan Ketentuan Sebagaimana Yang Telah Di Atur Oleh ".$_SESSION['hospitalname'];
-            $version           = "TNT – v.1.0.1";
+            $bodycheckcertificate= [];
+            $responsecheckcertificate = [];
 
-            $registrationid = Tilaka::uuidreenroll($useridentifier);
-            
-            if($registrationid!=null){
-                if($registrationid['success']){
-                    $datahash = CLIENT_ID_TILAKA.$consent_text.$version.$consent_timestamp;
-                    $hash     = hash_hmac('sha256', $datahash, CLIENT_SECRET_TILAKA);
+            $useridentifier    = $this->input->post("useridentifier");
+
+            $bodycheckcertificate['user_identifier']=$useridentifier;
+            $responsecheckcertificate = Tilaka::checkcertificateuser(json_encode($bodycheckcertificate));
+
+            if($responsecheckcertificate['success']){
+                if($responsecheckcertificate['status']===3){
+                    $consent_timestamp = date("Y-m-d H:i:s");
+                    $consent_text      = "Syarat dan Ketentuan Sebagaimana Yang Telah Di Atur Oleh ".$_SESSION['hospitalname'];
+                    $version           = "TNT – v.1.0.1";
         
-                    $body['registration_id']   = $registrationid['data'][0];
-                    $body['consent_text']      = $consent_text;
-                    $body['is_approved']       = true;
-                    $body['version']           = $version;
-                    $body['hash_consent']      = $hash;
-                    $body['consent_timestamp'] = $consent_timestamp;
-        
-                    $response = Tilaka::registerkyc(json_encode($body));
+                    $registrationid = Tilaka::uuidreenroll($useridentifier);
                     
-                    if($response['success']){
-                        $data['ISSUE_ID']=$response['data'][0];
-                        $this->md->updatedatauseridentifier($data,$useridentifier);
+                    if($registrationid!=null){
+                        if($registrationid['success']){
+                            $datahash = CLIENT_ID_TILAKA.$consent_text.$version.$consent_timestamp;
+                            $hash     = hash_hmac('sha256', $datahash, CLIENT_SECRET_TILAKA);
+                
+                            $body['registration_id']   = $registrationid['data'][0];
+                            $body['consent_text']      = $consent_text;
+                            $body['is_approved']       = true;
+                            $body['version']           = $version;
+                            $body['hash_consent']      = $hash;
+                            $body['consent_timestamp'] = $consent_timestamp;
+                
+                            $response = Tilaka::registerkyc(json_encode($body));
+                            
+                            if($response['success']){
+                                $data['ISSUE_ID']=$response['data'][0];
+                                $this->md->updatedatauseridentifier($data,$useridentifier);
+                            }
+            
+                            $json["responCode"]   = "00";
+                            $json["responHead"]   = "success";
+                            $json["responDesc"]   = "Data Di Temukan";
+                            $json['responResult'] = $response;
+                        }else{
+                            $json["responCode"]   = "01";
+                            $json["responHead"]   = "success";
+                            $json["responDesc"]   = "success";
+                            $json['responResult'] = $registrationid;
+                        }
+                    }else{
+                        $json["responCode"]   = "01";
+                        $json["responHead"]   = "error";
+                        $json["responDesc"]   = "Gagal Mendapatkan UUID Registration";
                     }
-    
-                    $json["responCode"]   = "00";
-                    $json["responHead"]   = "success";
-                    $json["responDesc"]   = "Data Di Temukan";
-                    $json['responResult'] = $response;
                 }else{
                     $json["responCode"]   = "01";
-                    $json["responHead"]   = "success";
-                    $json["responDesc"]   = "success";
-                    $json['responResult'] = $registrationid;
+                    $json["responHead"]   = "error";
+                    $json["responDesc"]   = $responsecheckcertificate['data'][0]['status'];
                 }
-            }else{
-                $json["responCode"]   = "01";
-                $json["responHead"]   = "error";
-                $json["responDesc"]   = "Gagal Mendapatkan UUID Registration";
             }
+            
             
 
             echo json_encode($json);
