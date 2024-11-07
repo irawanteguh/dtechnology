b9002d09411ca539a1da9c773f60aa41ae391d4e
Irawanteguh <teguhirawan.rsudpasarminggu@gmail.com>
2024-11-07 08:57:15 +0700

    Update

--- application/modules/tilakaV2/controllers/Registrasi.php
+++ application/modules/tilakaV2/controllers/Registrasi.php
@@ -58,30 +58,27 @@
             $response = Tilaka::checkcertificateuser(json_encode($body));
             
             if($response['success']){
-                if($response['success']){
-                    $data['CERTIFICATE']      = $response['status'];
-                    $data['CERTIFICATE_INFO'] = $response['message']['info'];
-                    
+                $data['CERTIFICATE']      = $response['status'];
+                $data['CERTIFICATE_INFO'] = $response['message']['info'];
+                
 
-                    if($response['status']==="4"){
-                        $data['USER_IDENTIFIER']  = "";
-                        $data['REGISTER_ID']      = "";
-                        $data['REVOKE_ID']        = "";
-                        $data['ISSUE_ID']         = "";
-                    }else{
-                        $data['REVOKE_ID']   = "";
-                        $data['ISSUE_ID']    = "";
+                if($response['status']==="4"){
+                    $data['USER_IDENTIFIER']  = "";
+                    $data['REGISTER_ID']      = "";
+                    $data['REVOKE_ID']        = "";
+                    $data['ISSUE_ID']         = "";
+                }else{
+                    $data['REVOKE_ID']   = "";
+                    $data['ISSUE_ID']    = "";
 
-                        if($response['status']==="3"){
-                            $data['START_ACTIVE'] = DateTime::createFromFormat('Y-m-d H:i:s', $response['data'][0]['start_active_date']);
-                            $data['EXPIRED_DATE'] = DateTime::createFromFormat('Y-m-d H:i:s', $response['data'][0]['expiry_date']);
-                        }
+                    if($response['status']==="3"){
+                        $data['START_ACTIVE'] = DateTime::createFromFormat('Y-m-d H:i:s', $response['data'][0]['start_active_date']);
+                        $data['EXPIRED_DATE'] = DateTime::createFromFormat('Y-m-d H:i:s', $response['data'][0]['expiry_date']);
                     }
-
-                    return var_dump($data);
-                    
-                    $this->md->updatedatauserid($data,$userid);
                 }
+
+                return var_dump($data);
+                $this->md->updatedatauserid($data,$userid);
 
                 $json["responCode"]   = "00";
                 $json["responHead"]   = "success";
