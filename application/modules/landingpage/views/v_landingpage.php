abaee1e822167037a5511100c80870768725704f
Irawanteguh <teguhirawan.rsudpasarminggu@gmail.com>
2024-11-12 10:34:59 +0700

    Update

--- application/modules/landingpage/views/v_landingpage.php
+++ application/modules/landingpage/views/v_landingpage.php
@@ -8,35 +8,35 @@
 				<li><a class = "nav-link scrollto" href = "#services">Services</a></li>
 				<li><a class = "nav-link scrollto" href = "#portfolio">Portfolio</a></li>
 				<li><a class = "nav-link scrollto" href = "#team">Team</a></li>
-				<li class="dropdown">
+				<!-- <li class="dropdown">
 					<a href="#"><span>Carrers</span> <i class="bi bi-chevron-down"></i></a>
 					<ul>
 						<li><a href="index.php/auth/signup">Sign Up</a></li>
 						<li><a href="index.php/auth/sign">Sign In</a></li>
 						<li><a href="#">Carrers List</a></li>
 					</ul>
-				</li>
+				</li> -->
 
-			<li class="dropdown">
-				<a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
-				<ul>
-					<li><a href="#">Drop Down 1</a></li>
-					<li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
-						<ul>
-							<li><a href="#">Deep Drop Down 1</a></li>
-							<li><a href="#">Deep Drop Down 2</a></li>
-							<li><a href="#">Deep Drop Down 3</a></li>
-							<li><a href="#">Deep Drop Down 4</a></li>
-							<li><a href="#">Deep Drop Down 5</a></li>
-						</ul>
-					</li>
-					<li><a href="#">Drop Down 2</a></li>
-					<li><a href="#">Drop Down 3</a></li>
-					<li><a href="#">Drop Down 4</a></li>
-				</ul>
-			</li>
-			<li><a class = "nav-link scrollto" href   = "#contact">Contact</a></li>
-			<li><a class = "getstarted scrollto" href = "index.php/auth/sign">Get Started</a></li>
+				<!-- <li class="dropdown">
+					<a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
+					<ul>
+						<li><a href="#">Drop Down 1</a></li>
+						<li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
+							<ul>
+								<li><a href="#">Deep Drop Down 1</a></li>
+								<li><a href="#">Deep Drop Down 2</a></li>
+								<li><a href="#">Deep Drop Down 3</a></li>
+								<li><a href="#">Deep Drop Down 4</a></li>
+								<li><a href="#">Deep Drop Down 5</a></li>
+							</ul>
+						</li>
+						<li><a href="#">Drop Down 2</a></li>
+						<li><a href="#">Drop Down 3</a></li>
+						<li><a href="#">Drop Down 4</a></li>
+					</ul>
+				</li> -->
+				<li><a class = "nav-link scrollto" href   = "#contact">Contact</a></li>
+				<li><a class = "getstarted scrollto" href = "index.php/auth/sign">Get Started</a></li>
 			</ul>
         	<i class="bi bi-list mobile-nav-toggle"></i>
       	</nav>
@@ -47,8 +47,8 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-lg-6 d-flex flex-column justify-content-center pt-4 pt-lg-0 order-2 order-lg-1" data-aos="fade-up" data-aos-delay="200">
-        <h1>Better Solutions for Your Healthcare Facility</h1>
-        <h2>We are a team of skilled developers creating integrated hospital applications with seamless functionality</h2>
+				<h1>Better Solutions for Your Healthcare Facility</h1>
+				<h2>We are a team of skilled developers creating integrated hospital applications with seamless functionality</h2>
 				<div class="d-flex justify-content-center justify-content-lg-start">
 					<a href="index.php/auth/sign" class="btn-get-started scrollto">Get Started</a>
 					<a href="https://www.youtube.com/watch?v=jDDaplaOz7Q" class="glightbox btn-watch-video"><i class="bi bi-play-circle"></i><span>Watch Video</span></a>
@@ -257,13 +257,13 @@
       <div class="container" data-aos="zoom-in">
 
         <div class="row">
-          <div class="col-lg-9 text-center text-lg-start">
-            <h3>Call To Action</h3>
-            <p> Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-          </div>
-          <div class="col-lg-3 cta-btn-container text-center">
-            <a class="cta-btn align-middle" href="#">Call To Action</a>
-          </div>
+			<div class="col-lg-9 text-center text-lg-start">
+				<h3>Call to Action</h3>
+				<p>Take the next step toward improved healthcare services. Connect with us to learn more about how our solutions can enhance patient care, streamline operations, and support your facility’s goals. Let’s make a difference together.</p>
+			</div>
+			<div class="col-lg-3 cta-btn-container text-center">
+				<a class="cta-btn align-middle" href="#">Call To Action</a>
+			</div>
         </div>
 
       </div>
@@ -476,8 +476,8 @@
 
           <div class="col-lg-4" data-aos="fade-up" data-aos-delay="100">
             <div class="box">
-              <h3>Free Plan</h3>
-              <h4><sup>$</sup>0<span>per month</span></h4>
+              <h3>Integrasi Tanda Tangan Elektronik</h3>
+              <h4><sup>Rp</sup>Call Us<span>instalation</span></h4>
               <ul>
                 <li><i class="bx bx-check"></i> Quam adipiscing vitae proin</li>
                 <li><i class="bx bx-check"></i> Nec feugiat nisl pretium</li>
@@ -491,7 +491,7 @@
 
           <div class="col-lg-4 mt-4 mt-lg-0" data-aos="fade-up" data-aos-delay="200">
             <div class="box featured">
-              <h3>Business Plan</h3>
+              <h3>Key Performance Index Employee</h3>
               <h4><sup>$</sup>29<span>per month</span></h4>
               <ul>
                 <li><i class="bx bx-check"></i> Quam adipiscing vitae proin</li>
@@ -600,19 +600,19 @@
               <div class="address">
                 <i class="bi bi-geo-alt"></i>
                 <h4>Location:</h4>
-                <p>A108 Adam Street, New York, NY 535022</p>
+                <p>Bogor, Jawa Barat, Indonesia</p>
               </div>
 
               <div class="email">
                 <i class="bi bi-envelope"></i>
                 <h4>Email:</h4>
-                <p>info@example.com</p>
+                <p>teguhirawan.dtechnology@gmail.com</p>
               </div>
 
               <div class="phone">
                 <i class="bi bi-phone"></i>
                 <h4>Call:</h4>
-                <p>+1 5589 55488 55s</p>
+                <p>081288646630</p>
               </div>
 
               <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621" frameborder="0" style="border:0; width: 100%; height: 290px;" allowfullscreen></iframe>
@@ -662,8 +662,8 @@
     <div class="container">
     <div class="row justify-content-center">
         <div class="col-lg-6">
-        <h4>Join Our Newsletter</h4>
-        <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna</p>
+          <h3>Join Our Newsletter</h3>
+          <p>Subscribe to our newsletter to receive the latest updates, healthcare insights, and exclusive offers directly to your inbox.</p>
         <form action="" method="post">
             <input type="email" name="email"><input type="submit" value="Subscribe">
         </form>
@@ -676,18 +676,21 @@
     <div class="container">
     <div class="row">
 
-        <div class="col-lg-3 col-md-6 footer-contact">
-        <h3>Arsha</h3>
+        <div class="col-lg-4 col-md-6 footer-contact">
+        <h3>DTechnology</h3>
         <p>
-            A108 Adam Street <br>
-            New York, NY 535022<br>
-            United States <br><br>
-            <strong>Phone:</strong> +1 5589 55488 55<br>
-            <strong>Email:</strong> info@example.com<br>
+            Bogor
+            <br>
+            Jawa Barat
+            <br>
+            Indonesia
+            <br><br>
+            <strong>Phone:</strong> 081288646630<br>
+            <strong>Email:</strong> teguhirawan.dtechnology@gmail.com<br>
         </p>
         </div>
 
-        <div class="col-lg-3 col-md-6 footer-links">
+        <div class="col-lg-2 col-md-6 footer-links">
         <h4>Useful Links</h4>
         <ul>
             <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
@@ -698,10 +701,10 @@
         </ul>
         </div>
 
-        <div class="col-lg-3 col-md-6 footer-links">
+        <div class="col-lg-2 col-md-6 footer-links">
         <h4>Our Services</h4>
         <ul>
-            <li><i class="bx bx-chevron-right"></i> <a href="#">Web Design</a></li>
+            <li><i class="bx bx-chevron-right"></i> <a href="#">Integration Tanda Tangan Elektronik</a></li>
             <li><i class="bx bx-chevron-right"></i> <a href="#">Web Development</a></li>
             <li><i class="bx bx-chevron-right"></i> <a href="#">Product Management</a></li>
             <li><i class="bx bx-chevron-right"></i> <a href="#">Marketing</a></li>
@@ -709,9 +712,9 @@
         </ul>
         </div>
 
-        <div class="col-lg-3 col-md-6 footer-links">
-        <h4>Our Social Networks</h4>
-        <p>Cras fermentum odio eu feugiat lide par naso tierra videa magna derita valies</p>
+        <div class="col-lg-4 col-md-6 footer-links">
+          <h3>Our Social Networks</h3>
+          <p>Connect with us on our social networks to stay updated with the latest news and services. We are here to engage and support you in any way we can.</p>
         <div class="social-links mt-3">
             <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
             <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
