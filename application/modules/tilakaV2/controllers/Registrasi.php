1d0f0a7479aea142ff228846d0318f50fe361e19
Irawanteguh <teguhirawan.rsudpasarminggu@gmail.com>
2024-11-13 15:28:01 +0700

    Update

--- application/modules/tilakaV2/controllers/Registrasi.php
+++ application/modules/tilakaV2/controllers/Registrasi.php
@@ -152,7 +152,7 @@
                         if($_GET['status'] === "Berhasil"){
                             $result   = $this->md->checkrevokeid($_SESSION['orgid'],$_GET['revoke_id']);
 
-                            $body['user_identifier']=$result->user_identifier;
+                            $body['user_identifier']=$result->USER_IDENTIFIER;
                             $response = Tilaka::checkcertificateuser(json_encode($body));
 
                             if($response['success']){
