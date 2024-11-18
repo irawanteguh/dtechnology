0fc0387cde845ea909a3248f57fb3f3f5ca3e157
Irawanteguh <teguhirawan.rsudpasarminggu@gmail.com>
2024-11-18 15:17:01 +0700

    Update

--- assets/js/tilakaV2/registrasi.js
+++ assets/js/tilakaV2/registrasi.js
@@ -283,13 +283,8 @@ function datakaryawan(){
                     }
 
                     if(result[i].CERTIFICATE==="3" && result[i].REVOKE_ID===""){
-                        if(result[i].status_expdate==="0"){
-                            statususer = "<td><div class='badge badge-light-success fw-bolder'>Sertifikat "+(result[i].CERTIFICATE_INFO ? result[i].CERTIFICATE_INFO : "")+"</div><div class='small'>Active : "+(result[i].startactive ? result[i].startactive : "")+" Expired :"+(result[i].expireddate ? result[i].expireddate : "")+"</div></td>";
-                            btnaction  = btncheckstatus+btnrevoke+btngantimfa;
-                        }else{
-                            statususer ="<td><div class='badge badge-light-danger fw-bolder'>Sertifikat Expired</div><div class='small'>Silakan Melakukan Pengajuan Kembali</div></td>";
-                            btnaction = btncheckstatus+btnreenroll;
-                        }   
+                        statususer = "<td><div class='badge badge-light-success fw-bolder'>Sertifikat "+(result[i].CERTIFICATE_INFO ? result[i].CERTIFICATE_INFO : "")+"</div><div class='small'>Active : "+(result[i].startactive ? result[i].startactive : "")+" Expired :"+(result[i].expireddate ? result[i].expireddate : "")+"</div></td>";
+                        btnaction  = btncheckstatus+btnrevoke+btngantimfa;   
                     }
 
                     if(result[i].CERTIFICATE==="3" && result[i].REVOKE_ID!=""){
