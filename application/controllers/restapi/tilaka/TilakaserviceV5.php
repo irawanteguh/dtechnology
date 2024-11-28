b1b23171539f1dd349a5596622c6050326d39863
Irawanteguh <teguhirawan.rsudpasarminggu@gmail.com>
2024-11-28 11:39:17 +0700

    Update

--- application/controllers/restapi/tilaka/TilakaserviceV5.php
+++ application/controllers/restapi/tilaka/TilakaserviceV5.php
@@ -204,7 +204,6 @@
                             $responseall['ResponseTilaka'] = $responsecheckcertificate;
                         }
                     }
-
                     
                     $responseservice[]=$responseall;
                 }
