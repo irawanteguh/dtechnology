41d112dcacc54748bcd054d410b24bd5f7557237
Irawanteguh <teguhirawan.rsudpasarminggu@gmail.com>
2024-12-10 09:29:23 +0700

    Update

--- assets/js/tilakaV2/registrasi.js
+++ assets/js/tilakaV2/registrasi.js
@@ -248,6 +248,11 @@ function datakaryawan(){
                         btnaction = btnreenroll;
                     }
 
+                    if(result[i].CERTIFICATE==="0" &&  result[i].USER_IDENTIFIER!="" && result[i].REVOKE_ID==="" && result[i].ISSUE_ID==="" && result[i].REASON_CODE==="0"){
+                        statususer = "<td><div class='badge badge-light-info fw-bolder'>"+(result[i].CERTIFICATE_INFO ? result[i].CERTIFICATE_INFO : "")+"</div><div class='small'>Mohon Menunggu Silakan Lakukan Pengecekan Secara Berkala</div></td>";
+                        btnaction  = btncheckstatus;
+                    }
+
                     if(result[i].CERTIFICATE==="0" &&  result[i].USER_IDENTIFIER==="" && result[i].REVOKE_ID===""){
                         statususer = "<td><div class='badge badge-light-info fw-bolder'>"+(result[i].CERTIFICATE_INFO ? result[i].CERTIFICATE_INFO : "")+"</div><div class='small'></div></td>";
                     }
