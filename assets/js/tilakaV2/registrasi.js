98a031fafaa5d8aab7077bd47296d180887e66dc
Irawanteguh <teguhirawan.rsudpasarminggu@gmail.com>
2024-12-10 09:50:04 +0700

    Update

--- assets/js/tilakaV2/registrasi.js
+++ assets/js/tilakaV2/registrasi.js
@@ -248,11 +248,6 @@ function datakaryawan(){
                         btnaction = btnreenroll;
                     }
 
-                    if(result[i].CERTIFICATE==="0" &&  result[i].USER_IDENTIFIER!="" && result[i].REVOKE_ID!="" && result[i].ISSUE_ID!="" && result[i].REASON_CODE==="0"){
-                        statususer = "<td><div class='badge badge-light-success fw-bolder'>Pengajuan Re Enroll Berhasil</div><div class='small'>Mohon Menunggu Silakan Lakukan Pengecekan Secara Berkala</div></td>";
-                        btnaction  = btncheckstatus;
-                    }
-
                     if(result[i].CERTIFICATE==="0" &&  result[i].USER_IDENTIFIER==="" && result[i].REVOKE_ID===""){
                         statususer = "<td><div class='badge badge-light-info fw-bolder'>"+(result[i].CERTIFICATE_INFO ? result[i].CERTIFICATE_INFO : "")+"</div><div class='small'></div></td>";
                     }
@@ -262,8 +257,13 @@ function datakaryawan(){
                             statususer = "<td><div class='badge badge-light-danger fw-bolder'>Account Sudah Di Revoke</div><div class='small'>Silakan Melakukan Pengajuan Sertifikat Tanda Tangan Elektronik</div></td>";
                             btnaction = btnreenroll;
                         }else{
-                            statususer = "<td><div class='badge badge-light-info fw-bolder'>"+(result[i].CERTIFICATE_INFO ? result[i].CERTIFICATE_INFO : "")+"</div><div class='badge badge-light-success fw-bolder'>Pengajuan Re Enroll Berhasil</div><div class='small'>Silakan Melakukan face recognition / Liveness</div></td>";
-                            btnaction  = btnverifikasienroll;
+                            if(result[i].REASON_CODE==="0"){
+                                statususer = "<td><div class='badge badge-light-success fw-bolder'>Pengajuan Re Enroll Berhasil</div><div class='small'>Mohon Menunggu Silakan Lakukan Pengecekan Secara Berkala</div></td>";
+                                btnaction  = btncheckstatus;
+                            }else{
+                                statususer = "<td><div class='badge badge-light-info fw-bolder'>"+(result[i].CERTIFICATE_INFO ? result[i].CERTIFICATE_INFO : "")+"</div><div class='badge badge-light-success fw-bolder'>Pengajuan Re Enroll Berhasil</div><div class='small'>Silakan Melakukan face recognition / Liveness</div></td>";
+                                btnaction  = btnverifikasienroll;
+                            }
                         }
                     }
 
