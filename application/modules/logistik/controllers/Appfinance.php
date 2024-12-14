c4c4de15e93617957638e9096efa452ab82f96b4
Irawanteguh <teguhirawan.rsudpasarminggu@gmail.com>
2024-12-14 11:13:31 +0700

    Update

--- application/modules/logistik/controllers/Appfinance.php
+++ application/modules/logistik/controllers/Appfinance.php
@@ -13,7 +13,7 @@
         }
 
         public function datarequest(){
-            $status = "and a.status in ('4','5','6','12','13','14','15','16')";
+            $status = "and a.status in ('4','5','6','12','13','14','15','16','17')";
             $result = $this->md->datarequest($_SESSION['orgid'],$status);
             
 			if(!empty($result)){
