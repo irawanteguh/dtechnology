60d00153732e18c45932afe7feafdda78c2581cd
Irawanteguh <teguhirawan.rsudpasarminggu@gmail.com>
2025-02-04 16:28:20 +0700

    Update

--- application/modules/monevfarmasi/models/Modelrawatjalan.php
+++ application/modules/monevfarmasi/models/Modelrawatjalan.php
@@ -12,7 +12,24 @@
                             23*0 obat_23,
                             (select nm_pasien from pasien where no_rkm_medis=x.norm)namapasien,
                             (select nm_dokter from dokter where kd_dokter=x.dokterid)namadokter,
-                            (select nm_poli from poliklinik where kd_poli=x.poliid)namapoli
+                            (select nm_poli from poliklinik where kd_poli=x.poliid)namapoli,
+                            reg+dokter+tindakan+obat+lab+rad totalbilling,
+                            case
+                                when x.poliid = 'IGDK' then 197600
+                                when x.poliid = 'U0001' then 319800
+                                when x.poliid = 'U0002' then 197600
+                                when x.poliid = 'U0003' then 197600
+                                when x.poliid = 'U0005' then 197600
+                                when x.poliid = 'U0011' then 257100
+                                when x.poliid = 'U0012' then 197600
+                                when x.poliid = 'U0007' then 197600
+                                when x.poliid = 'U0006' then 197600
+                                when x.poliid = 'U0004' then 197600
+                                when x.poliid = 'U0018' then 197600
+                                when x.poliid = 'U0053' then 131300
+                                else
+                                0 
+                            end hargaklaim
                         from(
                             select a.tgl_perawatan, no_rawat,
                                 jml,
@@ -21,11 +38,17 @@
                                 (select nama_brng from databarang where kode_brng=a.kode_brng)namaobat,
                                 (select no_rkm_medis from reg_periksa where no_rawat=a.no_rawat)norm,
                                 (select kd_dokter from reg_periksa where no_rawat=a.no_rawat)dokterid,
-                                (select kd_poli from reg_periksa where no_rawat=a.no_rawat)poliid
-                                
+                                (select kd_poli from reg_periksa where no_rawat=a.no_rawat)poliid,
+                                (select coalesce(sum(totalbiaya), 0) from billing where no_rawat=a.no_rawat and status='Registrasi' and no='Registrasi')reg,
+                                (select coalesce(sum(totalbiaya), 0) from billing where no_rawat=a.no_rawat and status='Dokter')dokter,
+                                (select coalesce(sum(totalbiaya), 0) from billing where no_rawat=a.no_rawat and status='Ralan Dokter')tindakan,
+                                (select coalesce(sum(totalbiaya), 0) from billing where no_rawat=a.no_rawat and status='Obat')obat,
+                                (select coalesce(sum(totalbiaya), 0) from billing where no_rawat=a.no_rawat and status='Laborat')lab,
+                                (select coalesce(sum(totalbiaya), 0) from billing where no_rawat=a.no_rawat and status='Radiologi')rad
                             from detail_pemberian_obat a
                             where a.status='Ralan'
-                            and   a.no_rawat in ('2025/01/06/000656','2025/01/09/000202','2025/01/08/000038','2025/01/02/000111')
+                            and   a.no_rawat in ('2025/01/06/000656','2025/01/09/000202','2025/01/08/000038','2025/01/02/000111','2025/01/10/000051')
+                            -- and   a.no_rawat in ('2025/01/06/000656','2025/01/10/000051')
                             order by poliid asc, dokterid asc, tgl_perawatan desc, no_rawat asc, namaobat asc
                         )x
                     ";
--- assets/js/monevfarmasi/rawatjalan.js
+++ assets/js/monevfarmasi/rawatjalan.js
@@ -18,7 +18,6 @@ function datamonev(){
             if (data.responCode === "00") {
                 var result = data.responResult;
         
-                // Variabel total untuk setiap poli-dokter
                 var totalModal, totalJual, totalObat7, totalObat23;
                 var tbodyContent = "";
         
@@ -26,12 +25,10 @@ function datamonev(){
                     var currentPoli = result[i].poliid + "-" + result[i].dokterid + "-" + result[i].norm;
         
                     if (lastpoli !== currentPoli) {
-                        // Jika bukan pertama kali, masukkan tbodyContent sebelumnya sebelum membuat tabel baru
                         if (lastpoli !== null) {
                             tableresult += tbodyContent;
         
-                            // Tambahkan total footer untuk poli sebelumnya
-                            tableresult += "<tfoot class='text-gray-600 fw-bold table-warning'>";
+                            tableresult += "</tbody><tfoot class='text-gray-600 fw-bold table-warning'>";
                             tableresult += "<tr>";
                             tableresult += "<td class='text-center' colspan='3'>TOTAL</td>";
                             tableresult += "<td class='text-end'>" + todesimal(totalModal) + "</td>";
@@ -41,10 +38,9 @@ function datamonev(){
                             tableresult += "<td class='pe-4 rounded-end text-end'>" + todesimal(totalObat23) + "</td>";
                             tableresult += "</tr>";
                             tableresult += "</tfoot>";
-        
-                            tableresult += "</tbody></table>";
+                            tableresult += "</table>";
 
-                            tableresult +="<div class='col-xl-6'>";
+                            tableresult +="<div class='col-xl-6 mb-20'>";
                                 tableresult +="<table class='table align-middle table-row-dashed fs-6 gy-2'>";
                                     tableresult +="<thead>";
                                         tableresult +="<tr class='fw-bolder text-muted bg-light'>";
@@ -57,13 +53,13 @@ function datamonev(){
                                             tableresult +="<td class='ps-4'>Klaim Obat Kronis</td>";
                                             tableresult +="<td>"+todesimal(totalObat23)+"</td>";
                                             tableresult +="<td>Registrasi</td>";
-                                            tableresult +="<td>15.000</td>";
+                                            tableresult +="<td>"+todesimal(biaya_reg)+"</td>";
                                         tableresult +="</tr>";
                                         tableresult +="<tr>";
                                             tableresult +="<td class='ps-4'>Klaim Rajal Biasa</td>";
-                                            tableresult +="<td>0</td>";
+                                            tableresult +="<td>"+todesimal(biaya_klaim)+"</td>";
                                             tableresult +="<td>Jasa Dokter</td>";
-                                            tableresult +="<td>50.000</td>";
+                                            tableresult +="<td>"+todesimal(biaya_dokter)+"</td>";
                                         tableresult +="</tr>";
                                         tableresult +="<tr>";
                                             tableresult +="<td></td>";
@@ -75,44 +71,88 @@ function datamonev(){
                                             tableresult +="<td></td>";
                                             tableresult +="<td></td>"; 
                                             tableresult +="<td class='ps-4'>Tindakan</td>";
-                                            tableresult +="<td>0</td>";
+                                            tableresult +="<td>"+todesimal(biaya_tindakan)+"</td>";
                                         tableresult +="</tr>";
                                         tableresult +="<tr>";
                                             tableresult +="<td></td>";
                                             tableresult +="<td></td>"; 
                                             tableresult +="<td class='ps-4'>Laboratorium</td>";
-                                            tableresult +="<td>0</td>";
+                                            tableresult +="<td>"+todesimal(biaya_lab)+"</td>";
                                         tableresult +="</tr>";
                                         tableresult +="<tr>";
                                             tableresult +="<td></td>";
                                             tableresult +="<td></td>"; 
                                             tableresult +="<td class='ps-4'>Radiologi</td>";
-                                            tableresult +="<td>0</td>";
+                                            tableresult +="<td>"+todesimal(biaya_rad)+"</td>";
                                         tableresult +="</tr>";
                                     tableresult +="</tbody>";
                                     tableresult +="<tfoot class='text-gray-600 fw-bold table-warning'>";
                                         tableresult +="<tr>";
                                             tableresult +="<td class='ps-4'>Total Klaim Per Episode</td>";
-                                            tableresult +="<td class='ps-4 rounded-start'>0</td>";
-                                            tableresult +="<td>Total Pengeluaran</td>";
-                                            tableresult +="<td class='pe-4 rounded-end'>"+todesimal((parseFloat(totalModal)+50000+15000))+"</td>";
+                                            tableresult +="<td class='ps-4'>"+todesimal(parseFloat(totalObat23)+parseFloat(biaya_klaim))+"</td>";
+                                            tableresult +="<td class='pe-4'>Total Pengeluaran</td>";
+                                            tableresult +="<td class='pe-4'>"+todesimal(biaya_totalbiiling)+"</td>";
+                                        tableresult +="</tr>";
+                                        tableresult +="<tr>";
+                                        if(parseFloat(biaya_totalbiiling) <  (parseFloat(totalObat23)+parseFloat(biaya_klaim))){
+                                            tableresult +="<td class='ps-4 table-success rounded-start rounded-end' colspan='4'>Potensi Keuntungan : "+todesimal(((parseFloat(totalObat23)+parseFloat(biaya_klaim))-biaya_totalbiiling))+"</td>";
+                                        }else{
+                                            tableresult +="<td class='ps-4 table-danger rounded-start rounded-end' colspan='4'>Potensi Kerugian : - "+todesimal(((parseFloat(totalObat23)+parseFloat(biaya_klaim))-biaya_totalbiiling))+"</td>";
+                                        }  
                                         tableresult +="</tr>";
                                     tableresult +="</tfoot>";
                                 tableresult +="</table>";
                             tableresult +="</div>";
                         }
         
-                        // Reset total dan tbodyContent untuk poli-dokter baru
-                        totalModal = 0;
-                        totalJual = 0;
-                        totalObat7 = 0;
-                        totalObat23 = 0;
+                        totalModal   = 0;
+                        totalJual    = 0;
+                        totalObat7   = 0;
+                        totalObat23  = 0;
                         tbodyContent = "";
         
                         tableresult += "<table class='table align-middle table-row-dashed fs-6 gy-2'>";
                         tableresult += "<thead>";
+                        tableresult += "<tr class='fw-bolder text-muted bg-light align-middle table-info'>";
+                        tableresult += "<th class='ps-4 rounded-start min-w-100px' rowspan='2'>Transaction</th>";
+                        tableresult += "<th class='text-start' rowspan='2'>Medical Record</th>";
+                        tableresult += "<th class='text-start min-w-250px' rowspan='2'>Patient Name</th>";
+                        tableresult += "<th class='text-start min-w-250px' rowspan='2'>Doctor</th>";
+                        tableresult += "<th class='pe-4 rounded-end text-center' colspan='7'>Bills</th>";
+                        tableresult += "</tr>";
+        
+                        tableresult += "<tr class='fw-bolder text-muted bg-light align-middle table-info'>";
+                        tableresult += "<th class='text-end'>Registration</th>";
+                        tableresult += "<th class='text-end'>Consultation</th>";
+                        tableresult += "<th class='text-end'>Treatment</th>";
+                        tableresult += "<th class='text-end'>Pharmacy</th>";
+                        tableresult += "<th class='text-end'>Laboratory</th>";
+                        tableresult += "<th class='text-end'>Radiology</th>";
+                        tableresult += "<th class='pe-4 rounded-end text-end'>Total</th>";
+                        tableresult += "</tr>";
+                        tableresult += "</thead>";
+        
+                        tableresult += "<tbody class='text-gray-600 fw-bold'>";
+                        tableresult += "<tr>";
+                        tableresult += "<td class='ps-4'><div>" + result[i].tgl_perawatan + "</div><div>" + result[i].no_rawat + "</div></td>";
+                        tableresult += "<td>" + result[i].norm + "</td>";
+                        tableresult += "<td>" + result[i].namapasien + "</td>";
+                        tableresult += "<td>" + result[i].namadokter + "</td>";
+                        tableresult += "<td class='text-end'>" + todesimal(result[i].reg) + "</td>";
+                        tableresult += "<td class='text-end'>" + todesimal(result[i].dokter) + "</td>";
+                        tableresult += "<td class='text-end'>" + todesimal(result[i].tindakan) + "</td>";
+                        tableresult += "<td class='text-end'>" + todesimal(result[i].obat) + "</td>";
+                        tableresult += "<td class='text-end'>" + todesimal(result[i].lab) + "</td>";
+                        tableresult += "<td class='text-end'>" + todesimal(result[i].rad) + "</td>";
+                        tableresult += "<td class='pe-4 text-end'>" + todesimal(result[i].totalbilling) + "</td>";
+                        tableresult += "</tr>";
+                        tableresult += "</tbody>";
+                        tableresult += "</table>";
+
+                        tableresult += "<table class='table align-middle table-row-dashed fs-6 gy-2'>";
+                        tableresult += "<thead>";
                         tableresult += "<tr class='fw-bolder text-muted bg-light'>";
-                        tableresult += "<th class='ps-4 rounded-start table-info' colspan='8'>" + result[i].namapoli + " [ " + result[i].namadokter + " ] Atasnama " + result[i].namapasien + " [" + result[i].norm + "]</th>";
+                        tableresult += "<th class='ps-4 rounded-start rounded-end table-info' colspan='8'>Pharmacy Analyst</th>";
                         tableresult += "</tr>";
                         tableresult += "<tr class='fw-bolder text-muted bg-light'>";
                         tableresult += "<th class='ps-4 rounded-start align-middle' rowspan='2'>Nama Obat</th>";
@@ -130,6 +170,15 @@ function datamonev(){
                         tableresult += "</tr>";
                         tableresult += "</thead>";
                         tableresult += "<tbody class='text-gray-600 fw-bold' id='" + currentPoli + "'>";
+
+                        biaya_reg          = result[i].reg;
+                        biaya_dokter       = result[i].dokter;
+                        biaya_tindakan     = result[i].tindakan;
+                        biaya_obat         = result[i].obat;
+                        biaya_lab          = result[i].lab;
+                        biaya_rad          = result[i].rad;
+                        biaya_totalbiiling = result[i].totalbilling;
+                        biaya_klaim        = result[i].hargaklaim;
                     }
         
                     // Update total per poli-dokter
@@ -137,6 +186,8 @@ function datamonev(){
                     totalJual   += parseFloat(result[i].totaljual) || 0;
                     totalObat7  += parseFloat(result[i].obat_7) || 0;
                     totalObat23 += parseFloat(result[i].obat_23) || 0;
+
+                    
         
                     // Tambahkan detail obat ke tbodyContent
                     tbodyContent += "<tr>";
@@ -157,7 +208,7 @@ function datamonev(){
                 if (tbodyContent !== "") {
                     tableresult += tbodyContent;
         
-                    tableresult += "<tfoot class='text-gray-600 fw-bold table-warning'>";
+                    tableresult += "</tbody><tfoot class='text-gray-600 fw-bold table-info'>";
                     tableresult += "<tr>";
                     tableresult += "<td class='ps-4 rounded-start text-center' colspan='3'>TOTAL</td>";
                     tableresult += "<td class='text-end'>" + todesimal(totalModal) + "</td>";
@@ -168,9 +219,9 @@ function datamonev(){
                     tableresult += "</tr>";
                     tableresult += "</tfoot>";
         
-                    tableresult += "</tbody></table>";
+                    tableresult += "</table>";
 
-                    tableresult +="<div class='col-xl-6'>";
+                    tableresult +="<div class='col-xl-6 mb-20'>";
                         tableresult +="<table class='table align-middle table-row-dashed fs-6 gy-2'>";
                             tableresult +="<thead>";
                                 tableresult +="<tr class='fw-bolder text-muted bg-light'>";
@@ -183,13 +234,13 @@ function datamonev(){
                                     tableresult +="<td class='ps-4'>Klaim Obat Kronis</td>";
                                     tableresult +="<td>"+todesimal(totalObat23)+"</td>";
                                     tableresult +="<td>Registrasi</td>";
-                                    tableresult +="<td>15.000</td>";
+                                    tableresult +="<td>"+todesimal(biaya_reg)+"</td>";
                                 tableresult +="</tr>";
                                 tableresult +="<tr>";
                                     tableresult +="<td class='ps-4'>Klaim Rajal Biasa</td>";
-                                    tableresult +="<td>0</td>";
+                                    tableresult +="<td>"+todesimal(biaya_klaim)+"</td>";
                                     tableresult +="<td>Jasa Dokter</td>";
-                                    tableresult +="<td>50.000</td>";
+                                    tableresult +="<td>"+todesimal(biaya_dokter)+"</td>";
                                 tableresult +="</tr>";
                                 tableresult +="<tr>";
                                     tableresult +="<td></td>";
@@ -201,27 +252,34 @@ function datamonev(){
                                     tableresult +="<td></td>";
                                     tableresult +="<td></td>"; 
                                     tableresult +="<td class='ps-4'>Tindakan</td>";
-                                    tableresult +="<td>0</td>";
+                                    tableresult +="<td>"+todesimal(biaya_tindakan)+"</td>";
                                 tableresult +="</tr>";
                                 tableresult +="<tr>";
                                     tableresult +="<td></td>";
                                     tableresult +="<td></td>"; 
                                     tableresult +="<td class='ps-4'>Laboratorium</td>";
-                                    tableresult +="<td>0</td>";
+                                    tableresult +="<td>"+todesimal(biaya_lab)+"</td>";
                                 tableresult +="</tr>";
                                 tableresult +="<tr>";
                                     tableresult +="<td></td>";
                                     tableresult +="<td></td>"; 
                                     tableresult +="<td class='ps-4'>Radiologi</td>";
-                                    tableresult +="<td>0</td>";
+                                    tableresult +="<td>"+todesimal(biaya_rad)+"</td>";
                                 tableresult +="</tr>";
                             tableresult +="</tbody>";
                             tableresult +="<tfoot class='text-gray-600 fw-bold table-warning'>";
                                 tableresult +="<tr>";
                                     tableresult +="<td class='ps-4'>Total Klaim Per Episode</td>";
-                                    tableresult +="<td class='ps-4 rounded-start'>0</td>";
-                                    tableresult +="<td>Total Pengeluaran</td>";
-                                    tableresult +="<td class='pe-4 rounded-end'>"+todesimal((parseFloat(totalModal)+50000+15000))+"</td>";
+                                    tableresult +="<td class='ps-4'>"+todesimal(parseFloat(totalObat23)+parseFloat(biaya_klaim))+"</td>";
+                                    tableresult +="<td class='pe-4'>Total Pengeluaran</td>";
+                                    tableresult +="<td class='pe-4'>"+todesimal(biaya_totalbiiling)+"</td>";
+                                tableresult +="</tr>";
+                                tableresult +="<tr>";
+                                if(parseFloat(biaya_totalbiiling) <  (parseFloat(totalObat23)+parseFloat(biaya_klaim))){
+                                    tableresult +="<td class='ps-4 table-success rounded-start rounded-end' colspan='4'>Potensi Keuntungan : "+todesimal(((parseFloat(totalObat23)+parseFloat(biaya_klaim))-biaya_totalbiiling))+"</td>";
+                                }else{
+                                    tableresult +="<td class='ps-4 table-danger rounded-start rounded-end' colspan='4'>Potensi Kerugian : - "+todesimal(((parseFloat(totalObat23)+parseFloat(biaya_klaim))-biaya_totalbiiling))+"</td>";
+                                }  
                                 tableresult +="</tr>";
                             tableresult +="</tfoot>";
                         tableresult +="</table>";
