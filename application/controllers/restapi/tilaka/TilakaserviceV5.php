38387e4e52dd120af27f6d479c861af851f18213
Irawanteguh <teguhirawan.rsudpasarminggu@gmail.com>
2025-02-17 15:24:54 +0700

    Update

--- application/controllers/restapi/tilaka/TilakaserviceV5.php
+++ application/controllers/restapi/tilaka/TilakaserviceV5.php
@@ -238,7 +238,7 @@
             $summaryresponse = [];
             $responseservice = [];
 
-            $status ="and a.status_sign ='3' limit 50;";
+            $status ="and a.status_sign ='3' limit 10;";
             $result = $this->md->listexecute(ORG_ID,$status);
 
             if(!empty($result)){
