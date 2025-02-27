759829736c539a9c0e45ae026d02d80ecd8fdbca
Irawanteguh <teguhirawan.rsudpasarminggu@gmail.com>
2025-02-27 21:09:34 +0700

    Update

--- application/controllers/restapi/tilaka/TilakaserviceV5.php
+++ application/controllers/restapi/tilaka/TilakaserviceV5.php
@@ -23,7 +23,7 @@
         public function uploadallfile_POST(){
             $responseservice = [];
 
-            $status          = "and   a.status_sign ='0' order by created_date asc limit 30;";
+            $status          = "and   a.status_sign ='0' order by created_date asc limit 50;";
             $result          = $this->md->uploaddata(ORG_ID,$status);
 
             if(!empty($result)){
