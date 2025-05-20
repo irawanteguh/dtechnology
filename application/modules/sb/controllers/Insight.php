e5c95016c6dd16897042b5c02902c7fbb8635c9e
Irawanteguh <teguhirawan.rsudpasarminggu@gmail.com>
2025-05-20 16:44:34 +0700

    update

--- application/modules/sb/controllers/Insight.php
+++ application/modules/sb/controllers/Insight.php
@@ -5,44 +5,13 @@
 		public function __construct(){
             parent:: __construct();
 			rootsystem::system();
-            $this->load->model("Modelinsight","md");
+            // $this->load->model("Modelinsight","md");
         }
 
 		public function index(){
-			$data = $this->loadcombobox();
-			$this->template->load("template/template-sidebar","v_insight",$data);
+			$this->template->load("template/template-sidebar","v_insight");
 		}
 
-
-        public function loadcombobox(){
-            $resultperiode = $this->md->periode();
-
-            $periode="";
-            foreach($resultperiode as $a ){
-                $periode.="<option value='".$a->periode."'>".$a->periode."</option>";
-            }
-
-            $data['periode'] = $periode;
-            return $data;
-		}
-
-        public function dataharian(){
-            $tahun = $this->input->post("periode");
-            $result = $this->md->dataharian($tahun);
-            
-			if(!empty($result)){
-                $json["responCode"]="00";
-                $json["responHead"]="success";
-                $json["responDesc"]="Data Successfully Found";
-                $json['responResult']=$result;
-            }else{
-                $json["responCode"]="01";
-                $json["responHead"]="info";
-                $json["responDesc"]="Data Failed to Find";
-            }
-
-            echo json_encode($json);
-        }
 
 
 	}
