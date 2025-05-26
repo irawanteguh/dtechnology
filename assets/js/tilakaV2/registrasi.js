bc00f36c1b1e48a8ee85652ec210b81748b04d57
Irawanteguh <teguhirawan.rsudpasarminggu@gmail.com>
2025-05-26 10:07:59 +0700

    update

--- assets/js/tilakaV2/registrasi.js
+++ assets/js/tilakaV2/registrasi.js
@@ -289,11 +289,6 @@ function datakaryawan(){
                         btnaction = btnverifpengajuan;
                     }
 
-                    if(result[i].REGISTER_ID!="" && result[i].CERTIFICATE==="1"){
-                        statususer ="<td><div class='badge badge-light-success fw-bolder'>Pengajuan berhasil</div><div class='small'>Silakan Melakukan face recognition / Liveness</div></td>";
-                        btnaction = btnverifpengajuan;
-                    }
-
                     if(result[i].CERTIFICATE==="0" &&  result[i].USER_IDENTIFIER!="" && result[i].REVOKE_ID==="" && result[i].ISSUE_ID==="" && result[i].status_expdate==="1"){
                         statususer ="<td><div class='badge badge-light-danger fw-bolder'>Sertifikat Expired</div><div class='small'>Silakan Melakukan Pengajuan Kembali</div></td>";
                         btnaction = btnreenroll;
