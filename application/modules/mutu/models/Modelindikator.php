05c05425ecb0e171b2458604b4cc4fadf7a80de3
Irawanteguh <teguhirawan.rsudpasarminggu@gmail.com>
2025-06-25 15:25:19 +0700

    update

---
+++ application/modules/mutu/models/Modelindikator.php
@@ -0,0 +1,19 @@
+<?php
+    class Modelindikator extends CI_Model{
+        
+        function masterindikator($orgid){
+
+            $query = "
+                        select a.mutu, definisi_operasional, numerator, denumerator, target, dimensi_mutu, tujuan, dasar_pemikiran, jenis_indikator, kriteria_inklusi, kriteria_eksklusi, formula
+                        from dt01_mutu_ms a
+                        where a.active='1'
+                        and   a.org_id='".$orgid."'
+                        order by mutu
+                    ";
+
+            $recordset = $this->db->query($query);
+            return $recordset->result();
+        }
+
+    }
+?>
