316321a8b4ddfa0ca2ca61e9c4eb2bd9aad13e9e
Irawanteguh <teguhirawan.rsudpasarminggu@gmail.com>
2025-07-09 11:38:22 +0700

    update

--- application/models/Modelwhatsapp.php
+++ application/models/Modelwhatsapp.php
@@ -7,7 +7,8 @@
                         select a.transaksi_id, a.no_hp, directory, body_1, body_2, body_3, body_4, body_5, body_6, body_7, body_8, body_9, body_10, type_file, document_name,
                             (select transaksi_id from dt01_whatsapp_device_ms where status='connected' and org_id=a.org_id and device_id=a.device_id)session
                         from dt01_whatsapp_broadcast_hd a
-                        where a.active='1'
+                        where a.org_id='".$orgid."'
+                        and   a.active='1'
                         and   a.status='0'
                         ".$limit."                     
                     ";
