580011652a999e79b83a81689ca8abf1078fbf48
Irawanteguh <teguhirawan.rsudpasarminggu@gmail.com>
2025-08-16 12:24:16 +0700

    update

--- application/views/template/toolbar.php
+++ application/views/template/toolbar.php
@@ -1,14 +1,14 @@
 <div class="toolbar" id="kt_toolbar">
 	<div id="kt_toolbar_container" class="container-fluid d-flex flex-stack">
 		
-		<div data-kt-swapper="true" data-kt-swapper-mode="prepend" data-kt-swapper-parent="{default: '#kt_content_container', 'lg': '#kt_toolbar_container'}" class="page-title d-flex align-items-center flex-wrap me-3 mb-5 mb-lg-0">
-			<?php
-				$breadcrumb = APPPATH . 'modules/' . $this->uri->segment(1) . '/breadcrumb/' . $this->uri->segment(2) . ".php";
-				if(file_exists($breadcrumb)){
-					include_once($breadcrumb);
-				}
-			?>
-		</div>
+		<?php
+			$breadcrumb = APPPATH . 'modules/' . $this->uri->segment(1) . '/breadcrumb/' . $this->uri->segment(2) . ".php";
+			if(file_exists($breadcrumb)){
+				echo '<div data-kt-swapper="true" data-kt-swapper-mode="prepend" data-kt-swapper-parent="{default: \'#kt_content_container\', \'lg\': \'#kt_toolbar_container\'}" class="page-title d-flex align-items-center flex-wrap me-3 mb-5 mb-lg-0">';
+				include_once($breadcrumb);
+				echo '</div>';
+			}
+		?>
 
 		<div class="d-flex align-items-center me-5 mb-3 mb-md-0">
 			<!-- <div class="d-flex align-items-center flex-shrink-0">
