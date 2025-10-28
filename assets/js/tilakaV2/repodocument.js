d42102aef608724e941ed0de9333e68f55f90c52
Irawanteguh <teguhirawan.rsudpasarminggu@gmail.com>
2025-10-28 23:18:31 +0700

    update

--- assets/js/tilakaV2/repodocument.js
+++ assets/js/tilakaV2/repodocument.js
@@ -65,66 +65,66 @@ function uploadfile(btn){
     });
 };
 
-function viewdoc(btn) {
-    var filename = $(btn).attr("data-dirfile");
-        filename = filename.replace('/www/wwwroot/', 'http://');
+// function viewdoc(btn) {
+//     var filename = $(btn).attr("data-dirfile");
+//         filename = filename.replace('/www/wwwroot/', 'http://');
 
-    // Menggunakan AJAX dengan metode GET untuk memeriksa apakah file ada
-    jQuery.ajax({
-        url: filename,
-        type: 'GET',
-        async: false,
-        success: function(data, textStatus, jqXHR) {
-            var viewfile = "<embed src='" +filename + "' width='100%' height='100%' type='application/pdf' id='view'>";
-            $("#viewdoc").html(viewfile);
+//     // Menggunakan AJAX dengan metode GET untuk memeriksa apakah file ada
+//     jQuery.ajax({
+//         url: filename,
+//         type: 'GET',
+//         async: false,
+//         success: function(data, textStatus, jqXHR) {
+//             var viewfile = "<embed src='" +filename + "' width='100%' height='100%' type='application/pdf' id='view'>";
+//             $("#viewdoc").html(viewfile);
             
-            $('#openInNewTabButton').data('filename', filename);
-        },
-        error: function(jqXHR, textStatus, errorThrown) {
-            var viewfile = `
-                <div class='alert alert-dismissible bg-light-info border border-info border-3 border-dashed d-flex flex-column flex-sm-row w-100 p-5 mb-10 fa-fade'>
-                    <span class='svg-icon svg-icon-2hx svg-icon-info me-4 mb-5 mb-sm-0'>
-                        <svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none'>
-                            <path opacity='0.3' d='M2 4V16C2 16.6 2.4 17 3 17H13L16.6 20.6C17.1 21.1 18 20.8 18 20V17H21C21.6 17 22 16.6 22 16V4C22 3.4 21.6 3 21 3H3C2.4 3 2 3.4 2 4Z' fill='black'></path>
-                            <path d='M18 9H6C5.4 9 5 8.6 5 8C5 7.4 5.4 7 6 7H18C18.6 7 19 7.4 19 8C19 8.6 18.6 9 18 9ZM16 12C16 11.4 15.6 11 15 11H6C5.4 11 5 11.4 5 12C5 12.6 5.4 13 6 13H15C15.6 13 16 12.6 16 12Z' fill='black'></path>
-                        </svg>
-                    </span>
-                    <div class='d-flex flex-column pe-0 pe-sm-10'>
-                        <h5 class='mb-1'>For Your Information</h5>
-                        <span>File Tidak Di Temukan, Silakan Periksa Kembali</span>
-                    </div>
-                </div>
-            `;
-            $("#viewdoc").html(viewfile);
-            $('#openInNewTabButton').data('filename', '');
-        }
-    });
+//             $('#openInNewTabButton').data('filename', filename);
+//         },
+//         error: function(jqXHR, textStatus, errorThrown) {
+//             var viewfile = `
+//                 <div class='alert alert-dismissible bg-light-info border border-info border-3 border-dashed d-flex flex-column flex-sm-row w-100 p-5 mb-10 fa-fade'>
+//                     <span class='svg-icon svg-icon-2hx svg-icon-info me-4 mb-5 mb-sm-0'>
+//                         <svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none'>
+//                             <path opacity='0.3' d='M2 4V16C2 16.6 2.4 17 3 17H13L16.6 20.6C17.1 21.1 18 20.8 18 20V17H21C21.6 17 22 16.6 22 16V4C22 3.4 21.6 3 21 3H3C2.4 3 2 3.4 2 4Z' fill='black'></path>
+//                             <path d='M18 9H6C5.4 9 5 8.6 5 8C5 7.4 5.4 7 6 7H18C18.6 7 19 7.4 19 8C19 8.6 18.6 9 18 9ZM16 12C16 11.4 15.6 11 15 11H6C5.4 11 5 11.4 5 12C5 12.6 5.4 13 6 13H15C15.6 13 16 12.6 16 12Z' fill='black'></path>
+//                         </svg>
+//                     </span>
+//                     <div class='d-flex flex-column pe-0 pe-sm-10'>
+//                         <h5 class='mb-1'>For Your Information</h5>
+//                         <span>File Tidak Di Temukan, Silakan Periksa Kembali</span>
+//                     </div>
+//                 </div>
+//             `;
+//             $("#viewdoc").html(viewfile);
+//             $('#openInNewTabButton').data('filename', '');
+//         }
+//     });
 
 
-    // if (responsefile === 200) {
-    //     var viewfile = "<embed src='" + filename + "' width='100%' height='100%' type='application/pdf' id='view'>";
-    //     $("#viewdoc").html(viewfile);
+//     // if (responsefile === 200) {
+//     //     var viewfile = "<embed src='" + filename + "' width='100%' height='100%' type='application/pdf' id='view'>";
+//     //     $("#viewdoc").html(viewfile);
         
-    //     $('#openInNewTabButton').data('filename', filename);
-    // } else {
-    //     var viewfile = `
-    //         <div class='alert alert-dismissible bg-light-info border border-info border-3 border-dashed d-flex flex-column flex-sm-row w-100 p-5 mb-10 fa-fade'>
-    //             <span class='svg-icon svg-icon-2hx svg-icon-info me-4 mb-5 mb-sm-0'>
-    //                 <svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none'>
-    //                     <path opacity='0.3' d='M2 4V16C2 16.6 2.4 17 3 17H13L16.6 20.6C17.1 21.1 18 20.8 18 20V17H21C21.6 17 22 16.6 22 16V4C22 3.4 21.6 3 21 3H3C2.4 3 2 3.4 2 4Z' fill='black'></path>
-    //                     <path d='M18 9H6C5.4 9 5 8.6 5 8C5 7.4 5.4 7 6 7H18C18.6 7 19 7.4 19 8C19 8.6 18.6 9 18 9ZM16 12C16 11.4 15.6 11 15 11H6C5.4 11 5 11.4 5 12C5 12.6 5.4 13 6 13H15C15.6 13 16 12.6 16 12Z' fill='black'></path>
-    //                 </svg>
-    //             </span>
-    //             <div class='d-flex flex-column pe-0 pe-sm-10'>
-    //                 <h5 class='mb-1'>For Your Information</h5>
-    //                 <span>File Tidak Di Temukan, Silakan Periksa Kembali</span>
-    //             </div>
-    //         </div>
-    //     `;
-    //     $("#viewdoc").html(viewfile);
-    //     $('#openInNewTabButton').data('filename', '');
-    // }
-}
+//     //     $('#openInNewTabButton').data('filename', filename);
+//     // } else {
+//     //     var viewfile = `
+//     //         <div class='alert alert-dismissible bg-light-info border border-info border-3 border-dashed d-flex flex-column flex-sm-row w-100 p-5 mb-10 fa-fade'>
+//     //             <span class='svg-icon svg-icon-2hx svg-icon-info me-4 mb-5 mb-sm-0'>
+//     //                 <svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none'>
+//     //                     <path opacity='0.3' d='M2 4V16C2 16.6 2.4 17 3 17H13L16.6 20.6C17.1 21.1 18 20.8 18 20V17H21C21.6 17 22 16.6 22 16V4C22 3.4 21.6 3 21 3H3C2.4 3 2 3.4 2 4Z' fill='black'></path>
+//     //                     <path d='M18 9H6C5.4 9 5 8.6 5 8C5 7.4 5.4 7 6 7H18C18.6 7 19 7.4 19 8C19 8.6 18.6 9 18 9ZM16 12C16 11.4 15.6 11 15 11H6C5.4 11 5 11.4 5 12C5 12.6 5.4 13 6 13H15C15.6 13 16 12.6 16 12Z' fill='black'></path>
+//     //                 </svg>
+//     //             </span>
+//     //             <div class='d-flex flex-column pe-0 pe-sm-10'>
+//     //                 <h5 class='mb-1'>For Your Information</h5>
+//     //                 <span>File Tidak Di Temukan, Silakan Periksa Kembali</span>
+//     //             </div>
+//     //         </div>
+//     //     `;
+//     //     $("#viewdoc").html(viewfile);
+//     //     $('#openInNewTabButton').data('filename', '');
+//     // }
+// }
 
 function dataupload(startDate,endDate){
     $.ajax({
@@ -272,16 +272,15 @@ function dataupload(startDate,endDate){
                     const tgljam    = item.tgljam || "";
 
                     let linkfile = "";
-                    const cleanUrl = url.replace(/index\.php\/?/, "");
-                    const filePath = item.source_file === "DTECHNOLOGY"
-                        ? `${cleanUrl}assets/document/${noFile}.pdf`
-                        : `${pathposttilaka}/${noFile}.pdf`;
+                    const filePath = item.source_file === "DTECHNOLOGY" 
+                    ? `${url}assets/document/${noFile}.pdf`
+                    : `${pathposttilaka}/${noFile}.pdf`;
 
                     // Tentukan link berdasarkan status_file & status_sign
                     if (item.status_sign === "0" && item.status_file === "0") {
                         linkfile = `<a href='#' data-bs-toggle='modal' data-bs-target='#modal_upload_document' data_dirfile='${item.no_file}' onclick='uploadfile(this)'>${noFile}</a>`;
                     } else {
-                        linkfile = `<a href='#' data-bs-toggle='modal' data-bs-target='#modal_view_pdf' data-dirfile='${filePath}' onclick='viewdoc(this)'>${noFile}</a>`;
+                        linkfile = `<a href='#' data-bs-toggle='modal' data-bs-target='#modal_view_pdf' data-dirfile='${filePath}' onclick='viewdocwithoutnote(this)'>${noFile}</a>`;
                     }
 
                     const descText = item.status_file === "0" && item.status_sign !== "5" && item.status_sign !== "99"
