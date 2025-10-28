69037f393f0bd40451309fa03305fbe932603d9f
Irawanteguh <teguhirawan.rsudpasarminggu@gmail.com>
2025-10-28 23:01:45 +0700

    update

--- application/models/Modeltilaka.php
+++ application/models/Modeltilaka.php
@@ -9,7 +9,6 @@
                         from dt01_gen_document_file_dt a
                         where a.active      = '1'
                         and   a.org_id      = '".$orgid."'
-                        and   a.status_file = '1'
                         and   a.status_sign = '0'
                         and   a.assign=(select nik from dt01_gen_user_data where org_id=a.org_id and active='1' and certificate='3' and nik=a.assign)
                         limit 10;
