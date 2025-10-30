4c820853bb04ff5a82755fbe57421552e58252e6
Irawanteguh <teguhirawan.rsudpasarminggu@gmail.com>
2025-10-30 14:30:16 +0700

    update

--- application/models/Modeltilaka.php
+++ application/models/Modeltilaka.php
@@ -58,13 +58,13 @@
             $query =
                     "
                         select a.no_file, filename, status_sign, assign, user_identifier, source_file,
-                                (select name          from dt01_gen_user_data   where org_id=a.org_id and active='1' and nik=a.assign)assignname,
-                                (select document_name from dt01_gen_document_ms where org_id=a.org_id and active='1' and JENIS_DOC=a.JENIS_DOC)jenisdocumen,
+                                (select name          from dt01_gen_user_data   where active='1' and nik=a.assign)assignname,
+                                (select document_name from dt01_gen_document_ms where active='1' and JENIS_DOC=a.JENIS_DOC)jenisdocumen,
                                 (select org_name      from dt01_gen_organization_ms where active='1' and org_id=a.org_id)orgname
                         from dt01_gen_document_file_dt a
                         where a.active      = '1'
                         and   a.assign      = '".$assign."'
-                        and   a.assign=(select nik from dt01_gen_user_data where org_id=a.org_id and active='1' and certificate='3' and nik=a.assign)
+                        and   a.assign=(select nik from dt01_gen_user_data where active='1' and certificate='3' and nik=a.assign)
                         ".$status."
                     ";
 
