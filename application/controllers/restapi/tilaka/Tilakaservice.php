35ee2ba621fcc8cd3da2357b16d85af4a74c0af9
Irawanteguh <teguhirawan.rsudpasarminggu@gmail.com>
2025-11-05 16:21:50 +0700

    update

--- application/controllers/restapi/tilaka/Tilakaservice.php
+++ application/controllers/restapi/tilaka/Tilakaservice.php
@@ -381,7 +381,7 @@
                                                     $listpdfsignatures['coordinate_x']    = $coordinatex;
                                                     $listpdfsignatures['coordinate_y']    = $coordinatey;
                                                     $listpdfsignatures['page_number']     = $page;
-                                                    $listpdfsignatures['qroption']        = "QRONLY";
+                                                    $listpdfsignatures['qr_option']       = "QRONLY";
                                         
                                                     if (CERTIFICATE === "PERSONAL") {
                                                         $listpdfsignatures['reason'] = "Signed on behalf of " . $files->orgname;
@@ -405,7 +405,7 @@
                                             $listpdfsignatures['coordinate_x']    = $coordinatex;
                                             $listpdfsignatures['coordinate_y']    = $coordinatey;
                                             $listpdfsignatures['page_number']     = $page;
-                                            $listpdfsignatures['qrcombine']       = "QRONLY";
+                                            $listpdfsignatures['qr_option']       = "QRONLY";
 
                                             if(CERTIFICATE==="PERSONAL"){
                                                 $listpdfsignatures['reason']       = "Signed on behalf of ".$files->orgname;
@@ -553,7 +553,7 @@
                                                 $listpdfsignatures['coordinate_x']    = $coordinatex;
                                                 $listpdfsignatures['coordinate_y']    = $coordinatey;
                                                 $listpdfsignatures['page_number']     = $page;
-                                                $listpdfsignatures['qroption']        = "QRONLY";
+                                                $listpdfsignatures['qr_option']        = "QRONLY";
                                     
                                                 if (CERTIFICATE === "PERSONAL") {
                                                     $listpdfsignatures['reason'] = "Signed on behalf of " . $files->orgname;
@@ -579,7 +579,7 @@
                                     $listpdfsignatures['coordinate_x']    = $coordinatex;
                                     $listpdfsignatures['coordinate_y']    = $coordinatey;
                                     $listpdfsignatures['page_number']     = $page;
-                                    $listpdfsignatures['qroption']        = "QRONLY";
+                                    $listpdfsignatures['qr_option']        = "QRONLY";
                                     if(CERTIFICATE==="PERSONAL"){
                                         $listpdfsignatures['reason']       = "Signed on behalf of ".$files->orgname;
                                     }
