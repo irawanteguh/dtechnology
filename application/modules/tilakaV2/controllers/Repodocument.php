197942535deaa83b5cb89ca02a15ff935c930a6e
Irawanteguh <teguhirawan.rsudpasarminggu@gmail.com>
2025-11-05 13:40:50 +0700

    update

--- application/modules/tilakaV2/controllers/Repodocument.php
+++ application/modules/tilakaV2/controllers/Repodocument.php
@@ -96,7 +96,7 @@
 
             $transid = generateuuid();
 
-            $config['upload_path']   = './assets/buktitl/';
+            $config['upload_path']   = './assets/document/';
             $config['allowed_types'] = 'pdf';
             $config['file_name']     = $transid."_SIGNER".$position;
             $config['overwrite']     = true;
