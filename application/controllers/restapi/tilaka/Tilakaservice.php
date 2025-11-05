eca66bbba57ebdcef2398d020a61065902ea273e
Irawanteguh <teguhirawan.rsudpasarminggu@gmail.com>
2025-11-05 14:16:00 +0700

    update

--- application/controllers/restapi/tilaka/Tilakaservice.php
+++ application/controllers/restapi/tilaka/Tilakaservice.php
@@ -269,8 +269,6 @@
                         if($filesize!=0){
                             $bodycheckcertificate['user_identifier']=$a->useridentifier;
                             $responsecheckcertificate = Tilaka::checkcertificateuser(json_encode($bodycheckcertificate));
-                            
-                            return var_dump($responsecheckcertificate);
 
                             if(isset($responsecheckcertificate['success'])){
                                 if($responsecheckcertificate['success']){
