e6278b8bf05ccd70bd76aed1a84e5c5047b8af49
irawanteguh <teguhirawan.rsudpasarminggu@gmail.com>
2025-11-12 16:24:55 +0700

    update

--- application/controllers/restapi/tilaka/Tilakaservice.php
+++ application/controllers/restapi/tilaka/Tilakaservice.php
@@ -811,18 +811,16 @@
                                 }
                             }
 
-                            return var_dump($response);
-
                             if($response['message']==="FAILED"){
                                 foreach($response['list_pdf'] as $listpdfs){
                                     $data        = [];
                                     $nofile      = preg_match('/_(.*?)\.pdf$/', $listpdfs['filename'], $matches) ? $matches[1] : '';
                                     
-                                    $data['STATUS_SIGN']     = "0";
+                                    $data['STATUS_SIGN']     = "99";
                                     $data['STATUS_FILE']     = "1";
                                     $data['REQUEST_ID']      = "";
                                     $data['LINK']            = "";
-                                    $data['NOTE']            = "";
+                                    $data['NOTE']            = $response['message'];
                                     $data['USER_IDENTIFIER'] = "";
                                     $data['URL']             = "";
                                     $this->md->updatefile($data,$nofile);
