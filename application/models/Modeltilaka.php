4490ba535b4352b36a6e2eb11abdb31a203d2ca8
Irawanteguh <teguhirawan.rsudpasarminggu@gmail.com>
2025-11-13 13:14:35 +0700

    update

--- application/models/Modeltilaka.php
+++ application/models/Modeltilaka.php
@@ -41,7 +41,7 @@
                                 (select user_identifier from dt01_gen_user_data   where org_id=a.org_id and active='1' and nik=a.assign)useridentifier
                         from dt01_gen_document_file_dt a
                         where a.active      = '1'
-                        and   a.status_sign = '5'
+                        and   a.status_sign = '0'
                         and   a.assign=(select nik from dt01_gen_user_data where org_id=a.org_id and active='1' and certificate='3' and nik=a.assign)
                         order by created_date asc
                         limit 10;
