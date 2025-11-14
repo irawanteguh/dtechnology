8137f83d9ee63edd0b7890082f04446af09aa9b1
irawanteguh <teguhirawan.rsudpasarminggu@gmail.com>
2025-11-14 09:48:28 +0700

    update

---
+++ application/controllers/restapi/tilaka/Tilakaregister.php
@@ -0,0 +1,73 @@
+<?php
+    defined('BASEPATH') or exit('No direct script access allowed');
+    date_default_timezone_set('Asia/Jakarta');
+    use Restserver\Libraries\REST_Controller;
+    require APPPATH . '/libraries/REST_Controller.php';
+    include FCPATH."assets/vendors/phpqrcode/qrlib.php";
+    include FCPATH."assets/vendors/pdfparse/Pdfparse.php";
+    require 'vendor/autoload.php';
+    use Smalot\PdfParser\Parser;
+    use setasign\Fpdi\Fpdi;
+    use setasign\Fpdi\PdfReader;
+
+    if(!function_exists('color')){
+        function color($name = null){
+            $colors = [
+                'reset'          => "\033[0m",
+                'black'          => "\033[30m",
+                'red'            => "\033[31m",
+                'green'          => "\033[32m",
+                'yellow'         => "\033[33m",
+                'blue'           => "\033[34m",
+                'magenta'        => "\033[35m",
+                'cyan'           => "\033[36m",
+                'white'          => "\033[37m",
+                'gray'           => "\033[90m",
+                'light_red'      => "\033[91m",
+                'light_green'    => "\033[92m",
+                'light_yellow'   => "\033[93m",
+                'light_blue'     => "\033[94m",
+                'light_magenta'  => "\033[95m",
+                'light_cyan'     => "\033[96m",
+                'light_white'    => "\033[97m",
+            ];
+
+            return $colors[$name] ?? $colors['reset'];
+        }
+    }
+
+    class Tilakaregister extends REST_Controller{
+        
+        public function __construct(){
+            parent::__construct();
+            $this->load->model("Modeltilaka","md");
+        }
+
+        public function headerlog(){
+            echo PHP_EOL;
+            echo color('cyan').str_pad("IDENTITY", 50).str_pad("USER IDENTIFIER", 42)."MESSAGE".PHP_EOL;
+        }
+
+        public function formatlog($identity, $useridentifier, $message, $colorIdentity = 'cyan', $colorUser = 'yellow', $colorMessage = 'white') {
+            $identityWidth       = 50;
+            $userIdentifierWidth = 42;
+
+            // Ambil warna sesuai parameter
+            $colorStartIdentity  = color($colorIdentity);
+            $colorStartUser      = color($colorUser);
+            $colorStartMessage   = color($colorMessage);
+            $reset               = color('reset');
+
+            // Susun log dengan padding dan warna
+            $formatted  = $colorStartIdentity . str_pad($identity, $identityWidth) . $reset;
+            $formatted .= $colorStartUser . str_pad($useridentifier, $userIdentifierWidth) . $reset;
+            $formatted .= $colorStartMessage . $message . $reset;
+
+            return $formatted . PHP_EOL;
+        }
+
+
+
+    }
+
+?>
