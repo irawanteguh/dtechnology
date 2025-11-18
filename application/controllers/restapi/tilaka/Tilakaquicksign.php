9438d6f18cae86259f9b0cf02ee96746ed8a9efa
irawanteguh <teguhirawan.rsudpasarminggu@gmail.com>
2025-11-18 22:29:18 +0700

    update

--- application/controllers/restapi/tilaka/Tilakaquicksign.php
+++ application/controllers/restapi/tilaka/Tilakaquicksign.php
@@ -574,7 +574,7 @@
                             if($response['message']==="PROCESS"){
                                 foreach($response['list_pdf'] as $listpdfs){
                                     $statusColor = "yellow";
-                                    $statusMsg   = $response['message']." | ".$response['status'][0]['num_signatures_done']."/".$response['status'][0]['num_signatures']." Document";
+                                    $statusMsg   = $response['message']." | ".$response['status'][0]['status']." | ".$response['status'][0]['num_signatures_done']."/".$response['status'][0]['num_signatures']." Document";
                                 }
                                 
                             }
