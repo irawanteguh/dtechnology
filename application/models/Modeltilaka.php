5beb73ffbbf8c4ea06800599b8d9b8b5bbe01ae8
Irawanteguh <teguhirawan.rsudpasarminggu@gmail.com>
2025-12-05 22:55:09 +0700

    update

--- application/models/Modeltilaka.php
+++ application/models/Modeltilaka.php
@@ -140,7 +140,7 @@
                                 (select name from dt01_gen_user_data   where org_id=a.org_id and active='1' and nik=a.assign)assignname
                         from dt01_gen_document_file_dt a
                         where a.active='1'
-                        and   a.status_sign='2'
+                        and   a.status_sign='3'
                         and a.assign='2102364'
                         limit 50;
                     ";
