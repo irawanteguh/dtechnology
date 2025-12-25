830e66bd1f144b77b84769272900abf9ed5cf23b
Irawanteguh <teguhirawan.rsudpasarminggu@gmail.com>
2025-12-25 10:02:58 +0700

    update

--- application/models/Modeltilaka.php
+++ application/models/Modeltilaka.php
@@ -165,7 +165,7 @@
                         from dt01_gen_document_file_dt a
                         where a.active='1'
                         and   a.status_sign='4'
-                        limit 10;
+                        limit 1;
                     ";
 
             $recordset = $this->db->query($query);
