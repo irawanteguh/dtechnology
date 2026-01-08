e4b6f3ed38155ad1823cc76d9eada29ffd747c4e
Irawanteguh <teguhirawan.rsudpasarminggu@gmail.com>
2026-01-08 21:46:53 +0700

    update

--- nodejs/servicetilakaquicksign.js
+++ nodejs/servicetilakaquicksign.js
@@ -144,7 +144,7 @@ async function callAPI(endpoint, method = "GET", body = null) {
 }
 
 async function runservices() {
-    // await callAPI("statusregister", "GET");
+    await callAPI("statusregister", "GET");
 	await callAPI("uploadallfile", "POST");
     await callAPI("requestsignquicksign", "POST");
     await callAPI("statussignquicksign", "POST");
