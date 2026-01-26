dadd482eae64d7264bed72249d6da973bdc96597
Irawanteguh <teguhirawan.rsudpasarminggu@gmail.com>
2026-01-26 23:58:22 +0700

    update

--- application/models/ModeltilakaV2.php
+++ application/models/ModeltilakaV2.php
@@ -93,7 +93,7 @@
                         from dt01_gen_document_file_dt a
                         where a.active='1'
                         and   a.status_sign in ('2','3')
-                        limit 1;
+                        limit 50;
                     ";
 
             $recordset = $this->db->query($query);
