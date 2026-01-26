9f488ea3129a789adeaceb4cedfc405b91cae887
Irawanteguh <teguhirawan.rsudpasarminggu@gmail.com>
2026-01-27 00:33:32 +0700

    update

--- application/models/ModeltilakaV2.php
+++ application/models/ModeltilakaV2.php
@@ -93,7 +93,7 @@
                         where a.active='1'
                         -- and   a.status_sign in ('2','3')
                         and   a.status_sign='3'
-                        limit 50;
+                        limit 10;
                     ";
 
             $recordset = $this->db->query($query);
