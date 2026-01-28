85e96792b18b741b0806b727ec79caf5ceff1c9d
Irawanteguh <teguhirawan.rsudpasarminggu@gmail.com>
2026-01-28 07:26:02 +0700

    update

--- application/models/ModeltilakaV2.php
+++ application/models/ModeltilakaV2.php
@@ -77,6 +77,7 @@
                             ON FIND_IN_SET(u.nik, REPLACE(d.assign, ';', ',')) > 0
                         WHERE d.active = '1'
                         AND d.status_sign = '1'
+                        and d.ASSIGN ='2101372'
                         GROUP BY d.org_id, d.no_file, d.filename, d.source_file
                         LIMIT 10;
                     ";
