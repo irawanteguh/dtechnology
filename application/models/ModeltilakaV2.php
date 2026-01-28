db26ee72c240a3bce607774177f32d5ea160d5de
Irawanteguh <teguhirawan.rsudpasarminggu@gmail.com>
2026-01-28 08:01:44 +0700

    update

--- application/models/ModeltilakaV2.php
+++ application/models/ModeltilakaV2.php
@@ -79,7 +79,7 @@
                         AND d.status_sign = '1'
                         and d.ASSIGN ='2101372'
                         GROUP BY d.org_id, d.no_file, d.filename, d.source_file
-                        LIMIT 10;
+                        LIMIT 5;
                     ";
 
             // echo $query; // debug bila perlu
