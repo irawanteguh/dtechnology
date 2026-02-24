66bfa6367ea8153124a606c3df38f9841f86695b
Irawanteguh <teguhirawan.rsudpasarminggu@gmail.com>
2026-02-24 09:08:32 +0700

    update

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -2,7 +2,7 @@
     defined('BASEPATH') OR exit('No direct script access allowed');
 
     $autoload['packages']  = array(APPPATH.'third_party/MX/');
-    $autoload['libraries'] = array('database','session','template','tilaka','tilakaplus','mekari','satusehat','inacbg','Gatewaywhatsapp');
+    $autoload['libraries'] = array('database','session','template','tilaka','mekari','satusehat','inacbg','Gatewaywhatsapp');
     $autoload['drivers']   = array();
     $autoload['helper']    = array('url','file','rootsystem','curl','authorization','jwt');
     $autoload['config']    = array();
