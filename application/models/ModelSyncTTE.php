4931dd7e37d6406b1e4d1e76215915c9e90d1539
Irawanteguh <teguhirawan.rsudpasarminggu@gmail.com>
2026-03-11 11:40:06 +0700

    update

--- application/models/ModelSyncTTE.php
+++ application/models/ModelSyncTTE.php
@@ -11,6 +11,7 @@
                         and   a.assign<>''
                         and   a.status_sign in ('1','0')
                         and   a.no_file not in (select no_file from dt01_sign_document_dt)
+                        order by created_date desc
                         limit 10;
                     ";
 
