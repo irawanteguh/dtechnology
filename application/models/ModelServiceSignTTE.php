49d9f4ba30d68b6814031ea0461393ec23c37596
Irawanteguh <teguhirawan.rsudpasarminggu@gmail.com>
2026-07-17 15:57:00 +0700

    update

--- application/models/ModelServiceSignTTE.php
+++ application/models/ModelServiceSignTTE.php
@@ -84,6 +84,7 @@
                         FROM dt01_sign_document_dt a
                         WHERE a.status_sign = '0'
                         AND a.provider_sign = 'Tilaka'
+                        and a.signer_id='1173'
                         LIMIT 20;
                     ";
 
